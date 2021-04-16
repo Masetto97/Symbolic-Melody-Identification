284443b65108158291286f20b0100837212e4276
Eduardo Mora <32511969+Masetto97@users.noreply.github.com>
2021-04-16 10:27:59 +0200

    Update server.py
    LIMPIEZA DE CODIGO Y AÑADIDA FUNCIONALIDADES

--- server.py
+++ server.py
@@ -1,13 +1,19 @@
 import socket, pickle
 import os
 import requests
+
+#CREAMOS EL SOCKET
 s = socket.socket()
 s.bind(('', 5000))
 s.listen(1)
 c,a = s.accept()
+
+#OBTENEMOS EL TITULO DE LA CANCION Y CALCULAMOS LAS SALIDAS
 titulo = c.recv(512)
-titulo_final='./'+titulo+'_procesado.mid'
+titulo_final='./Salidas_pruebas_TFM/'+titulo+'_procesado.mid'
 print(titulo_final)
+
+#RECIBIMOS EL FICHERO
 filetodown = open('./recibido.mid', "wb")
 while True:
    aux = c.recv(512)
@@ -22,15 +28,17 @@ filetodown.close()
 print("ARCHIVO RECIBIDO")
 c.send("Thank you for connecting.")
 
+#PROCESAMOS EL ARCHIVO Y LO GUARDAMOS EN EL DIRECTORIO CORRESPONDIENTE
 print("PROCESANDO ARCHIVO")
-#os.system('conda run -n IA ./terminal_client.py --model model.pkl --extract  recibido.mid ' + titulo_final)
-#os.system('ls')
+os.system('conda run -n IA ./terminal_client.py --model model.pkl --extract  recibido.mid ' + titulo_final)
 
+#ENVIAMOS EL ARCHIVO A DOCKER WEB PARA QUE LO GUARDE EN LA BBDD
 ficheros = {'file1': open('./Salidas_pruebas_TFM/albinoni_adagio_procesado.mid', 'rb')}
 r = requests.post('http://web:5000/procesado')
 
-#x = requests.post('172.18.0.4:5000/index')
-
+#BORRAMOS EL ARCHIVO RECIBIDO AL ESTAR YA PROCESADO Y ENVIADO
+os.system('rm recibido.mid')
+          
 c.shutdown(2)
 c.close()
 s.close()
