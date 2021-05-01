3aafbbb16a381cbe496f6e06e7090a43d9f14e91
Eduardo Mora <32511969+Masetto97@users.noreply.github.com>
2021-05-01 21:27:50 +0200

    bucle infinito

--- server.py
+++ server.py
@@ -5,41 +5,43 @@ import requests
 #CREAMOS EL SOCKET
 s = socket.socket()
 s.bind(('', 5000))
-s.listen(1)
-c,a = s.accept()
-
-#OBTENEMOS EL TITULO DE LA CANCION Y CALCULAMOS LAS SALIDAS
-titulo = c.recv(512)
-titulo_procesado = titulo+'_procesado.mid'
-ruta='./Salidas_pruebas_TFM/'
-print(titulo_procesado)
-
-#RECIBIMOS EL FICHERO
-filetodown = open('recibido.mid', "wb")
 while True:
-   aux = c.recv(512)
-   if aux[-3:] == 'fin':
-        aux1 = aux[:-3]
-        #data = pickle.loads(aux1)
-        filetodown.write(aux1)
-        break
-   #data = pickle.loads(aux)
-   filetodown.write(aux)
-filetodown.close()
-print("ARCHIVO RECIBIDO")
-c.send("Thank you for connecting.")
+   s.listen(1)
+   c,a = s.accept()
 
-#PROCESAMOS EL ARCHIVO Y LO GUARDAMOS EN EL DIRECTORIO CORRESPONDIENTE
-print("PROCESANDO ARCHIVO")
-os.system('conda run -n IA ./terminal_client.py --model model.pkl --extract  recibido.mid ' + ruta+titulo_procesado)
+   #OBTENEMOS EL TITULO DE LA CANCION Y CALCULAMOS LAS SALIDAS
+   titulo = c.recv(512)
+   titulo_procesado = titulo+'_procesado.mid'
+   ruta='./Salidas_pruebas_TFM/'
+   print(titulo_procesado)
 
-#ENVIAMOS EL ARCHIVO A DOCKER WEB PARA QUE LO GUARDE EN LA BBDD
-fichero = {'file1': open(ruta+titulo_procesado, 'rb')}
-r = requests.post('http://web:5000/procesado', files=fichero)
+   #RECIBIMOS EL FICHERO
+   filetodown = open('recibido.mid', "wb")
+   while True:
+      aux = c.recv(512)
+      if aux[-3:] == 'fin':
+           aux1 = aux[:-3]
+           #data = pickle.loads(aux1)
+           filetodown.write(aux1)
+           break
+      #data = pickle.loads(aux)
+      filetodown.write(aux)
+   filetodown.close()
+   print("ARCHIVO RECIBIDO")
+   c.send("Thank you for connecting.")
 
-#BORRAMOS EL ARCHIVO RECIBIDO AL ESTAR YA PROCESADO Y ENVIADO
-os.system('rm recibido.mid')
-os.system('ls')        
+   #PROCESAMOS EL ARCHIVO Y LO GUARDAMOS EN EL DIRECTORIO CORRESPONDIENTE
+   print("PROCESANDO ARCHIVO")
+   os.system('conda run -n IA ./terminal_client.py --model model.pkl --extract  recibido.mid ' + ruta+titulo_procesado)
+
+   #ENVIAMOS EL ARCHIVO A DOCKER WEB PARA QUE LO GUARDE EN LA BBDD
+   fichero = {'file1': open(ruta+titulo_procesado, 'rb')}
+   r = requests.post('http://web:5000/procesado', files=fichero)
+
+   #BORRAMOS EL ARCHIVO RECIBIDO AL ESTAR YA PROCESADO Y ENVIADO
+   os.system('rm recibido.mid')
+   os.system('ls')
+   
 c.shutdown(2)
 c.close()
 s.close()
